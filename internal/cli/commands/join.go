33f2a36462a4d7d15663e8175ba60ad073805498
SaschaRunge <sascharunge89@yahoo.de>
2026-05-25 22:14:11 +0200

    add selector functionality to /join

--- internal/cli/commands/join.go
+++ internal/cli/commands/join.go
@@ -3,7 +3,9 @@ package commands
 import (
 	"fmt"
 
+	"github.com/SaschaRunge/llm-local/internal/cli/commands/selector"
 	"github.com/SaschaRunge/llm-local/internal/core"
+	"github.com/SaschaRunge/llm-local/internal/database"
 	"github.com/SaschaRunge/llm-local/internal/scenes"
 )
 
@@ -13,8 +15,8 @@ func (c *join) Name() string { return "/join" }
 func (c *join) Description() string {
 	return "Starts the chat interface. Will load or create the chat [chat_name]."
 }
-func (c *join) Usage() string     { return fmt.Sprintf("%s [chat_name]", c.Name()) }
-func (c *join) MinArguments() int { return 1 }
+func (c *join) Usage() string     { return fmt.Sprintf("%s [OPTIONAL:chat_name]", c.Name()) }
+func (c *join) MinArguments() int { return 0 }
 func (c *join) MaxArguments() int { return 1 }
 
 func (c *join) CanExecute(commandCtx core.CommandContext) bool {
@@ -22,27 +24,41 @@ func (c *join) CanExecute(commandCtx core.CommandContext) bool {
 }
 
 func (c *join) Execute(commandCtx core.CommandContext) (core.Result, error) {
-	chats, err := commandCtx.Runtime.Store().DBQueries.GetChatsLikeName(commandCtx.Runtime.Context(), commandCtx.Args[0])
-	if err != nil {
-		return core.Result{}, err
-	}
-	if len(chats) == 0 {
-		return core.Result{}, fmt.Errorf("no chat with name %q", commandCtx.Args[0])
+	var chats []database.Chat
+	var selectedChat database.Chat
+	var err error
+	if len(commandCtx.Args) == 1 {
+		chats, err = commandCtx.Runtime.Store().DBQueries.GetChatsLikeName(commandCtx.Runtime.Context(), commandCtx.Args[0])
+		if err != nil {
+			return core.Result{}, err
+		}
+		if len(chats) == 0 {
+			return core.Result{}, fmt.Errorf("no chat with name %q", commandCtx.Args[0])
+		}
+		if len(chats) > 1 {
+			return core.Result{}, fmt.Errorf("multiple results for %q, please try again", commandCtx.Args[0])
+		}
+		selectedChat = chats[0]
+	} else {
+		chats, err := commandCtx.Runtime.Store().DBQueries.GetAllChats(commandCtx.Runtime.Context())
+		if err != nil {
+			return core.Result{}, err
+		}
+		if len(chats) == 0 {
+			return core.Result{}, fmt.Errorf("no valid chats available")
+		}
+
+		selectedChat = selector.Select(
+			chats,
+			func(chat database.Chat) string { return chat.Name },
+			commandCtx.Runtime.GetInput)
+
+		if selectedChat.Name == "" {
+			return core.Result{}, err
+		}
 	}
 
-	/*// TODO: selection from multiple personas
-	personas, err := ctx.cli.DBQueries.GetPersonasLikeName(ctx.cli.context, ctx.args[1])
-	if err != nil {
-		return nil, err
-	}
-	if len(personas) == 0 {
-		return nil, fmt.Errorf("no persona with name %q", ctx.args[1])
-	}*/
-
-	sceneChat, err := scenes.NewSceneChat(commandCtx.Runtime, chats[0]) /*, scenes.Character{
-		ID:   personas[0].ID,
-		Name: personas[0].Name,
-	})*/
+	sceneChat, err := scenes.NewSceneChat(commandCtx.Runtime, selectedChat)
 	if err != nil {
 		return core.Result{}, err
 	}
